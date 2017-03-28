a5a97c8175531e47854a89fe7d85bf51c768d9d1
diogojr2d <diogojrdesouza@gmail.com>
2017-03-28 09:10:10 -0300

    Add Exercise 2: Fork

---
+++ ex2fork.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[]) {
+	pid_t pid = getpid();
+	for (int i = 0; i < 4; i++) {
+		if (pid > 0) {
+			pid = fork();
+			if (pid == 0) {
+				printf("Processo filho %d \n", getpid());
+				break;
+			} else if (pid > 0) {
+				printf("Processo pai %d criou %d \n", getpid(), pid);
+			}
+		}
+	}
+
+//	while(1);
+	while(wait(NULL) != -1);
+	return 0;
+}
