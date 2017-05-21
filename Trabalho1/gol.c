ec7751b161e15cef09af53fc87089d3504a2744e
diogojr2d <diogojrdesouza@gmail.com>
2017-05-21 11:19:20 -0300

    Add Trabalho1 (Game of Life)

---
+++ Trabalho1/gol.c
@@ -0,0 +1,201 @@
+/*
+* The Game of Life
+*
+* a cell is born, if it has exactly three neighbours
+* a cell dies of loneliness, if it has less than two neighbours
+* a cell dies of overcrowding, if it has more than three neighbours
+* a cell survives to the next generation, if it does not die of loneliness
+* or overcrowding
+*
+* In this version, a 2D array of ints is used.  A 1 cell is on, a 0 cell is off.
+* The game plays a number of steps (given by the input), printing to the screen each time.  'x' printed
+* means on, space means off.
+*
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+typedef unsigned char cell_t;
+int numThreads = 4;
+int currentStep = 0;
+cell_t** prev;
+cell_t** next;
+cell_t** tmp;
+pthread_barrier_t barrier;
+
+cell_t ** allocate_board (int size) {
+  cell_t ** board = (cell_t **) malloc(sizeof(cell_t*)*size);
+  int	i;
+  for (i=0; i<size; i++)
+  board[i] = (cell_t *) malloc(sizeof(cell_t)*size);
+  return board;
+}
+
+void free_board (cell_t ** board, int size) {
+  int     i;
+  for (i=0; i<size; i++)
+  	free(board[i]);
+  free(board);
+}
+
+
+/* return the number of on cells adjacent to the i,j cell */
+int adjacent_to (cell_t ** board, int size, int i, int j) {
+  int	k, l, count=0;
+
+  int sk = (i>0) ? i-1 : i;
+  int ek = (i+1 < size) ? i+1 : i;
+  int sl = (j>0) ? j-1 : j;
+  int el = (j+1 < size) ? j+1 : j;
+
+  for (k=sk; k<=ek; k++) {
+  	for (l=sl; l<=el; l++) {
+  		count+=board[k][l];
+  	}
+  }
+  count-=board[i][j];
+
+  return count;
+}
+
+void play (cell_t ** board, cell_t ** newboard, int size, int start, int end) {
+  int	i, j, a, c;
+  /* for each cell, apply the rules of Life */
+  for (c=start; c<end; ++c) {
+    i = c%size;
+    j = c/size;
+    a = adjacent_to (board, size, i, j);
+    if (a == 2) newboard[i][j] = board[i][j];
+    if (a == 3) newboard[i][j] = 1;
+    if (a < 2) newboard[i][j] = 0;
+    if (a > 3) newboard[i][j] = 0;
+  }
+}
+
+/* print the life board */
+void print (cell_t ** board, int size) {
+  int	i, j;
+  /* for each row */
+  for (j=0; j<size; j++) {
+    /* print each column position... */
+    for (i=0; i<size; i++)
+    printf ("%c", board[i][j] ? 'x' : ' ');
+    /* followed by a carriage return */
+    printf ("\n");
+  }
+}
+
+/* read a file into the life board */
+void read_file (FILE * f, cell_t ** board, int size) {
+  int	i, j;
+  char	*s = (char *) malloc(size+10);
+
+  /* read the first new line (it will be ignored) */
+  fgets (s, size+10,f);
+
+  /* read the life board */
+  for (j=0; j<size; j++) {
+    /* get a string */
+    fgets (s, size+10,f);
+    /* copy the string to the life board */
+    for (i=0; i<size; i++)
+    	board[i][j] = s[i] == 'x';
+
+  }
+}
+
+void* thrWork(void* args_) {
+  int* args = (int*)args_;
+  int thrID = args[0];
+  int thrRange = args[1];
+  int size = args[2];
+  int steps = args[3];
+
+  // Calcula células que serão atualizadas por cada thread
+  int start = thrID * thrRange;
+  int end = start + thrRange;
+  if (thrID == numThreads - 1) {
+    end += (size*size)%numThreads;
+  }
+
+  int s = 0;
+  while (currentStep < steps) {
+    play (prev,next,size, start, end);  
+
+    // aguarda que todas as threads calculem o novo tabuleiro
+    s = pthread_barrier_wait(&barrier);
+
+    // apenas uma thread atualiza e imprime o tabuleiro novo
+    if (s == PTHREAD_BARRIER_SERIAL_THREAD) {
+      #ifdef DEBUG
+        printf("%d ----------\n", currentStep + 1);
+        print (next,size);
+      #endif
+      tmp = next;
+      next = prev;
+      prev = tmp;
+      currentStep++;
+    }
+
+    // aguarda a atualização, antes de partir para o próximo step
+    pthread_barrier_wait(&barrier);
+  }
+
+  pthread_exit(NULL);
+}
+
+int main (int argc, char** argv) {
+  // Verifica argumento (número de threads)
+  if (argc == 2) {
+    numThreads = atoi(argv[1]);
+    if (numThreads < 1) {
+      printf("Erro: numero de threads deve ser positivo.\n");
+      return 0;
+    }
+  }
+
+  // Lê arquivo e aloca tabuleiros
+  int size, steps;
+  FILE    *f;
+  f = stdin;
+  fscanf(f,"%d %d", &size, &steps);
+  prev = allocate_board (size);
+  read_file (f, prev,size);
+  fclose(f);
+  next = allocate_board (size);
+  
+  // Imprime passos intermediários
+  #ifdef DEBUG
+  printf("Initial:\n");
+  print(prev,size);
+  #endif
+
+  // Inicializa threads
+  int thrRange = size*size/numThreads;
+  pthread_t threads[numThreads];
+  pthread_barrier_init(&barrier, NULL, numThreads);
+
+  for (int i = 0; i < numThreads; ++i) {
+    int args[3];
+    args[0] = i;
+    args[1] = thrRange;
+    args[2] = size;
+    args[3] = steps;
+    pthread_create(&threads[i], NULL, thrWork, (void*)args);
+  }
+
+  for (int i = 0; i < numThreads; ++i) {
+    pthread_join(threads[i], NULL);
+  }
+
+  // Imprime resultado
+  #ifdef RESULT
+    printf("Final:\n");
+    print (prev,size);
+  #endif
+
+  free_board(prev,size);
+  free_board(next,size);
+}
